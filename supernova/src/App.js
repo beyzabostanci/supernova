6a5d31e13556b576fd4e21f8200c42691856dc84
beyzabostanci <beyza_bostanc@hotmail.com>
2022-11-20 18:49:14 +0300

    blowing effect added with bg image

---
+++ supernova/src/App.js
@@ -0,0 +1,14 @@
+import "./index.css";
+import Dot from "./components/Dot/Dot";
+
+function App() {
+  return (
+    <div className="App">
+    {/*<img src="../strats.jpg"  alt=""/>*/}
+    <Dot/>
+    
+    </div>
+  );
+}
+
+export default App;
